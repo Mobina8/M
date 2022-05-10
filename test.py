a160d57bac09be3c5af934373b68acb9315be1b1
mobina8072 <104554294+Mobina8@users.noreply.github.com>
2022-05-10 12:08:55 +0430

    Add files via upload

---
+++ test.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+94*900
+
+
+# In[2]:
+
+
+563+200
+
+
+# In[3]:
+
+
+5000*1000
+
+
+# In[4]:
+
+
+3333-5555
+
+
+# In[5]:
+
+
+64545/2
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('autocall', '')
+
+
+# In[10]:
+
+
+get_ipython().run_line_magic('autocall', '[mode]')
+
+
+# In[11]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[12]:
+
+
+get_ipython().run_line_magic('cd', '')
+
+
+# In[13]:
+
+
+get_ipython().run_line_magic('dhist', '')
+
+
+# In[14]:
+
+
+get_ipython().run_line_magic('edit', '')
+
+
+# In[15]:
+
+
+get_ipython().run_line_magic('env', '')
+
+
+# In[17]:
+
+
+get_ipython().run_line_magic('env', 'va')
+
+
+# In[18]:
+
+
+get_ipython().run_line_magic('env', 'va wild')
+
+
+# In[19]:
+
+
+get_ipython().run_line_magic('gui', '')
+
+
+# In[20]:
+
+
+get_ipython().run_line_magic('gui', 'wx')
+
+
+# In[21]:
+
+
+get_ipython().run_line_magic('gui', 'qt4 | qt')
+
+
+# In[22]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[25]:
+
+
+get_ipython().run_line_magic('matplotlib', '')
+
+
+# In[26]:
+
+
+get_ipython().run_line_magic('notbook', '')
+
+
+# In[27]:
+
+
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[28]:
+
+
+get_ipython().run_line_magic('precision', '')
+
+
+# In[29]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[30]:
+
+
+get_ipython().run_line_magic('pylab', '')
+
+
+# In[35]:
+
+
+get_ipython().run_line_magic('recall', '')
+
+
+# In[ ]:
+
+
+C:\Users\Mreza
+
+
+# In[36]:
+
+
+get_ipython().run_line_magic('run', '')
+
+
+# In[37]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[38]:
+
+
+get_ipython().run_line_magic('who', '')
+
+
+# In[ ]:
+
+
+
+
